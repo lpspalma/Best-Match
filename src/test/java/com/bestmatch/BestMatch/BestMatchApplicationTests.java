77b964ff0bdc92b1658599d99a086ef475221b2b
Lucas Palma <lu.palma.silva@gmail.com>
2024-01-12 20:46:04 -0300

    Base Structure

---
+++ src/test/java/com/bestmatch/BestMatch/BestMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestmatch.BestMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
